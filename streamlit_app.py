645a533dd8050eaf864bb780a6751db7b70f5320
abdulrehmankapadia <106053874+abdulrehmankapadia@users.noreply.github.com>
2023-09-24 22:12:56 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,106 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
+import pandas as pd
+import psycopg2
 
-"""
-# Welcome to Streamlit!
+# Define the function for Page 1
+def page1():
+    st.title("Expense Tracker - Page 1")
+    st.write("Select your name:")
+    selected_username = st.selectbox("Select the Name", ["Abdulreman", "Ahmed", "Khalilurrehman", "Zakera"])
+    
+    if st.button("Next"):
+        # Store selected username in session state
+        st.session_state.username = selected_username
+        # Navigate to the next page
+        st.session_state.page_number = 2  # Set the page number for next page
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+def page2():
+    st.title("Expense Tracker - Page 2")
+    selected_username = st.session_state.username
+    
+    if not selected_username:
+        st.error("Invalid access. Please go back and select your name.")
+        return
+    
+    # Retrieve the expense_df from session state
+    expense_df = st.session_state.get('expense_df', pd.DataFrame(columns=["Username", "Date", "Category", "Item", "Quantity", "Amount", "Mode of payment"]))
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+    st.write(f"Your username from Page 1: {selected_username}")
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+    # Get expense details
+    date = st.date_input("Select date of the Expense:")
+    spent_on_category = st.selectbox("Spent On Category:", ["Grocery", "Fruits", "Vegetables", "Maintenance", "Fees", "Travelling", "Others"])
+    spent_on_item = st.text_input("Spent On Item:")
+    qty = st.number_input("Quantity (QTY):", min_value=0)
+    amount = st.number_input("Amount (in Rupees):", min_value=0)
+    options = st.selectbox("Mode of payment", ["Credit", "Debit Card", "Bank transfer", "Cash"], index=3)
+
+    if st.button("Add Expense"):
+    # Create a new row as a dictionary
+        new_expense = {
+            "Username": [selected_username],
+            "Date": [date],
+            "Category": [spent_on_category],
+            "Item": [spent_on_item],
+            "Quantity": [qty],
+            "Amount": [amount],
+            "Mode of payment": [options]
+        }
+
+        # Convert the new expense to a DataFrame and append it to expense_df
+        new_expense_df = pd.DataFrame(new_expense)
+        expense_df = pd.concat([expense_df, new_expense_df], ignore_index=True)
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+        # Update the expense_df in session state
+        st.session_state.expense_df = expense_df
+        st.success("Expense added successfully!")
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+        # Display a preview of entered expenses
+        st.write("Preview of Expenses:")
+        st.write(expense_df)
 
-    points_per_turn = total_points / num_turns
+        # Insert the data into PostgreSQL
+        insert_into_postgres(expense_df)
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+# Function to insert DataFrame data into PostgreSQL
+def insert_into_postgres(df):
+    try:
+        conn = psycopg2.connect(
+            database="jbwbxpex",
+            user="jbwbxpex",
+            password="NHnZbQ8GwX8JoKHokHGif1l5nsmBeWDA",
+            host="drona.db.elephantsql.com",
+            port="5432"
+        )
+        cursor = conn.cursor()
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+        for index, row in df.iterrows():
+            cursor.execute("INSERT INTO your_table (username, date, category, item, quantity, amount, mode_of_payment) VALUES (%s, %s, %s, %s, %s, %s, %s)",
+                           (row['Username'], row['Date'], row['Category'], row['Item'], row['Quantity'], row['Amount'], row['Mode of payment']))
+
+        conn.commit()
+        cursor.close()
+        conn.close()
+    except Exception as e:
+        st.error(f"An error occurred while inserting into PostgreSQL: {str(e)}")
+
+
+
+# Define the main Streamlit app
+def main():
+    st.set_page_config(page_title="Expense Tracker", page_icon=":money_with_wings:")
+    
+    # Check the page number to determine which page to show
+    page_number = st.session_state.get('page_number', 1)
+
+    if page_number == 1:
+        page1()
+    elif page_number == 2:
+        page2()
+    else:
+        st.error("Invalid page number.")
+
+# Call the main function to run the app
+if __name__ == "__main__":
+    main()
